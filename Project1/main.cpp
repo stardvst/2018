f312559fbdb3e1a633f76a6381a661d947efacd7
Lilia <lilia.manukyan@hotmail.com>
2018-05-06 16:06:18 +0400

    c++17 fs dir traversal

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,34 +1,65 @@
 #include <iostream>
+#include <chrono>
+#include <filesystem>
+#include <ctime>
 
-//template <int N>
-//constexpr int fibonacci()
-//{
-//	return fibonacci<N - 1>() + fibonacci<N - 2>();
-//}
-//
-//template <>
-//constexpr int fibonacci<1>()
-//{
-//	return 1;
-//}
-//
-//template <>
-//constexpr int fibonacci<0>()
-//{
-//	return 0;
-//}
+namespace fs = std::experimental::filesystem;
 
-template <int N>
-constexpr int fibonacci()
+uintmax_t ComputeFileSize(const fs::path &pathToCheck)
 {
-	if constexpr (N >= 2)
-		return fibonacci<N - 1>() + fibonacci<N - 2>();
-	return N;
+	if (fs::exists(pathToCheck) &&
+		fs::is_regular_file(pathToCheck))
+	{
+		auto err = std::error_code {};
+		auto filesize = fs::file_size(pathToCheck, err);
+		if (filesize != static_cast<uintmax_t>(-1))
+			return filesize;
+	}
+	return static_cast<uintmax_t>(-1);
+}
+
+void CoutLMT(const fs::path &entry)
+{
+	auto timeEntry = fs::last_write_time(entry);
+	time_t cftime = std::chrono::system_clock::to_time_t(timeEntry);
+	std::cout << std::asctime(std::localtime(&cftime));
+}
+
+void DisplayFileInfo(const fs::directory_entry &entry, const std::string &lead, const fs::path &filename)
+{
+	if (filename.extension() == ".docx")
+		std::cout << lead << "[*] " << filename.filename() << " (" << entry.path().is_absolute() << ")\n";
+}
+
+void DisplayDirTree(const fs::path &pathToShow, int level)
+{
+	if (fs::exists(pathToShow) && fs::is_directory(pathToShow))
+	{
+		auto lead = std::string(level * 3, ' ');
+		for (const auto &entry : fs::directory_iterator(pathToShow))
+		{
+			auto filename = entry.path().filename();
+			if (fs::is_directory(entry.status()) && entry.path().filename() != ".git")
+			{
+				std::cout << lead << "[+] " << filename.filename() << '\n';
+				DisplayDirTree(entry, level + 1);
+				std::cout << '\n';
+			}
+			else if (fs::is_regular_file(entry.status()))
+				DisplayFileInfo(entry, lead, filename);
+			else
+				std::cout << lead << " [?]" << filename.filename() << '\n';
+		}
+	}
 }
 
 int main()
 {
-	std::cout << fibonacci<8>();
+	auto path = "C:\\Windows\\bfsvc.exe";
+	std::cout << ComputeFileSize(path) << '\n';
+	CoutLMT(path);
+
+	DisplayDirTree("C:\\Users\\liliam\\Documents", 0);
 
 	std::cin.get();
 	return 0;
