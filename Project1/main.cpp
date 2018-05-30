ba1ed395c53f5752ab4c245c772591e72228ef84
Lilia <lilia.manukyan@hotmail.com>
2018-05-30 22:04:37 +0400

    temporary return rvo

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
-#include <vector>
 
-std::vector<bool> GetPrimes(const int n)
+struct C
 {
-	std::vector<bool> primes(n + 1, true);
-	for (auto i = 2; i <= n; ++i)
-		if (primes[i])
-			for (auto j = 2; j * i <= n; ++j)
-				primes[j * i] = false;
-	return primes;
+	C() = default;
+	C(const C&) { std::cout << "A copy was made.\n"; }
+};
+
+C f()
+
+{
+	// calls copy ctor
+	/*C c;
+	return c;*/
+
+	// doesn't call copy ctor
+	return C();
 }
 
 int main()
 {
-	int a;
-	std::cout << "Enter number, Henrik!: ";
-	std::cin >> a;
-	auto primes = GetPrimes(a);
-	for (std::size_t i = 2; i < primes.size(); ++i)
-		if (primes[i])
-			std::cout << i << ", ";
+	std::cout << "Hello World!\n";
+	C obj = f();
 
 	std::cin.get();
 	return 0;
