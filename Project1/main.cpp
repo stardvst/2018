96f242617e3a9ba0d994df3756643bafa8b5594d
Lilia <lilia.manukyan@hotmail.com>
2018-05-30 21:48:56 +0400

    = delete vs private

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
 
-struct Base
+class Foo
 {
-	virtual void f() { std::cout << "Base::f()" << std::endl; }
+public:
+	static Foo *create()
+	{
+		return new Foo;
+	}
+	~Foo() {}
+
+	// error message says "... is a deleted function" - very clear
+	Foo(const Foo &) = delete;
+	Foo &operator=(const Foo &) = delete;
+
+private:
+
+	// error message says "... is private" - not clear whether it's just private or not allowed to do
+	Foo(const Foo &) {}
+	Foo &operator=(const Foo &) {}
+
+	Foo() { std::cout << "created\n"; }
 };
 
-struct Derived :Base
+void f(Foo foo)
 {
-	virtual void f() override { std::cout << "Derived::f()" << std::endl; }
-};
-
-void SomeMethod(Base *object, void (Base::*ptr)())
-{
-	(object->*ptr)();
 }
-
-//void SomeMethod(Base &object, void (Base::*ptr)())
-//{
-//    (object.*ptr)();    
-//}
 
 int main()
 {
-	Base b;
-	Derived d;
-	Base* p = &b;
-
-	//SomeMethod(b, &Base::f); // calls Base::f()
-	//SomeMethod(d, &Base::f); // calls Derived::f()
-	
-	SomeMethod(p, &Base::f); // calls Base::f()
-	p = &d;
-	SomeMethod(p, &Base::f); // calls Derived::f()
+	Foo *foo = Foo::create();
+	f(*foo);
 
 	std::cin.get();
 	return 0;
