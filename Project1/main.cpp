00be3153226c188800b717b51645092b7a880777
Lilia <lilia.manukyan@hotmail.com>
2018-06-06 23:10:02 +0400

    dtors for static class members are called too

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,26 +1,63 @@
 #include <iostream>
 
+class B
+{
+public:
+	B(int b) : m_b(b)
+	{
+		std::cout << "ctor B " << b << "\n";
+	}
+
+	~B()
+	{
+		std::cout << "dtor B " << m_b << "\n";
+	}
+
+	void print()
+	{
+		std::cout << "B::print " << m_b << "\n";
+	}
+
+private:
+	int m_b;
+};
+
 class A
 {
 public:
-	A()
+	A(int a) : m_a(a)
 	{
-		std::cout << "ctor\n";
+		std::cout << "ctor A " << a << "\n";
 	}
 
 	~A()
 	{
-		std::cout << "dtor\n";
+		std::cout << "dtor A " << m_a << "\n";
 	}
+
+private:
+	static B b;
+	int m_a;
 };
 
+B A::b(7);
+
 // global object
-A a;
+A a(1);
 
 int main()
 {
-	auto a = new A;
-	static auto b = new A;
+	auto a = new A(2);
+	static auto b = new A(3);
+
+	int aa, bb;
+	std::cin >> aa;
+	std::cin >> bb;
+	if (aa + bb < 10)
+	{
+		static B b(4);
+		b.print();
+	}
 
 	std::cin.get();
 
