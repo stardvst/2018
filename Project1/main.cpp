06206e1267a6111314c1b67436b37895f3c43e71
Lilia <lilia.manukyan@hotmail.com>
2018-06-17 22:51:04 +0400

    assignment op. returns A&

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,42 +1,62 @@
 #include <iostream>
-#include <vector>
 
-template <typename T>
-void f(T param)
+class Widget
 {
-	std::cout << __FUNCSIG__ << ": " << typeid(param).name() << '\n';
-}
+public:
+	Widget(int i) : m_int(i)
+	{
+		std::cout << "ctor for " << i << ": " << this << '\n';
+	}
 
-template <typename T>
-void f(std::initializer_list<T> param)
-{
-	std::cout << __FUNCSIG__ << ": " << typeid(param).name() << '\n';
-}
+	~Widget()
+	{
+		std::cout << "dtor for " << m_old << ": " << this << '\n';
+	}
 
-// template, not type-deduction
-//auto createInitList()
-//{
-//	return { 1,2,3 };
-//}
+	Widget(const Widget &rhs)
+	{
+		m_int = rhs.m_int;
+		std::cout << "copy ctor for " << rhs.m_old << ": " << this << '\n';
+	}
+
+	const Widget &operator=(const Widget &rhs)
+	{
+		m_old = m_int;
+		m_int = rhs.m_int;
+		return *this;
+	}
+
+	int get() const
+	{
+		return m_int;
+	}
+
+private:
+	int m_int;
+	int m_old;
+};
 
 int main()
 {
-	auto x1 = 27;
-	auto x2(27);
-	auto x3 = { 27 };
-	auto x4{ 27 };
+	Widget w1(4);
+	Widget w2(7);
+	Widget w3(8);
+	Widget w4(1);
 
-	auto x5 = { 4,6,7 };
-	f(x5);
+	std::cout << "w1: " << w1.get() << '\n';
+	std::cout << "w2: " << w2.get() << '\n';
+	std::cout << "w3: " << w3.get() << '\n';
+	std::cout << "w4: " << w4.get() << "\n\n";
 
-	//f({ 11, 23, 9 });
+	w4 = w3 = w2 = w1;
 
+	std::cout << "w1: " << w1.get() << '\n';
+	std::cout << "w2: " << w2.get() << '\n';
+	std::cout << "w3: " << w3.get() << '\n';
+	std::cout << "w4: " << w4.get() << '\n';
 
-	// template, not type-deduction
-	std::vector<int> v;
-	auto resetV = [&v](const auto &newValue) { v = newValue; };
-
-	resetV({ 1,2,3 });
+	// can't with const &
+	Widget &ref = w1 = w2;
 
 	std::cin.get();
 	return 0;
