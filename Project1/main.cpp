580caebc322683387de6f8c109f118b56481320c
Lilia <lilia.manukyan@hotmail.com>
2018-06-18 22:02:49 +0400

    Base doesn't copy its all parts, derived doesn't copy base part

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,62 +1,46 @@
 #include <iostream>
 
-class Widget
+class Base
 {
 public:
-	Widget(int i) : m_int(i)
-	{
-		std::cout << "ctor for " << i << ": " << this << '\n';
-	}
+	Base(int a = 0, int b = 0) : m_a(a), m_b(b) { }
+	int a() const { return m_a; }
+	int b() const { return m_b; }
 
-	~Widget()
-	{
-		std::cout << "dtor for " << m_old << ": " << this << '\n';
-	}
-
-	Widget(const Widget &rhs)
-	{
-		m_int = rhs.m_int;
-		std::cout << "copy ctor for " << rhs.m_old << ": " << this << '\n';
-	}
-
-	const Widget &operator=(const Widget &rhs)
-	{
-		m_old = m_int;
-		m_int = rhs.m_int;
-		return *this;
-	}
-
-	int get() const
-	{
-		return m_int;
-	}
+	// doesn't copy b, oops!
+	Base(const Base &base) : m_a(base.m_a) { }
 
 private:
-	int m_int;
-	int m_old;
+	int m_a;
+	int m_b;
+};
+
+class Derived : public Base
+{
+public:
+	Derived(int a, int b, int c) : Base(a, b), m_c(c) {}
+	int c() const { return m_c; }
+
+	// doesn't copy base part
+	Derived(const Derived &derived) : m_c(derived.m_c) {}
+
+private:
+	int m_c;
 };
 
 int main()
 {
-	Widget w1(4);
-	Widget w2(7);
-	Widget w3(8);
-	Widget w4(1);
+	Base b1(1, 2);
+	Base b2 = b1;
 
-	std::cout << "w1: " << w1.get() << '\n';
-	std::cout << "w2: " << w2.get() << '\n';
-	std::cout << "w3: " << w3.get() << '\n';
-	std::cout << "w4: " << w4.get() << "\n\n";
+	std::cout << "b1: " << b1.a() << ' ' << b1.b() << '\n';
+	std::cout << "b2: " << b2.a() << ' ' << b2.b() << '\n';
 
-	w4 = w3 = w2 = w1;
+	Derived d1(10, 11, 12);
+	Derived d2 = d1;
 
-	std::cout << "w1: " << w1.get() << '\n';
-	std::cout << "w2: " << w2.get() << '\n';
-	std::cout << "w3: " << w3.get() << '\n';
-	std::cout << "w4: " << w4.get() << '\n';
-
-	// can't with const &
-	Widget &ref = w1 = w2;
+	std::cout << "d1: " << d1.a() << ' ' << d1.b() << ' ' << d1.c() << '\n';
+	std::cout << "d2: " << d2.a() << ' ' << d2.b() << ' ' << d2.c() << '\n';
 
 	std::cin.get();
 	return 0;
