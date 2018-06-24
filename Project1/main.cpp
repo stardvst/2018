9dfa58cabfadab1e8e44907cb21fb3c897f6bbc0
Lilia <lilia.manukyan@hotmail.com>
2018-06-24 23:15:22 +0400

    gsl::not_null

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,65 +1,21 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
+#include <gsl/gsl>
 
-struct Number
+int *ret(int n)
 {
-	static Number *instance();
-
-	static void setType(const std::string &t)
-	{
-		type = t;
-		delete inst;
-		inst = nullptr;
-	}
-
-	virtual void setValue(int in) { value = in; }
-	virtual int getValue() const { return value; }
-
-protected:
-	int value;
-	Number() { std::cout << "ctor: "; }
-
-private:
-	static std::string type;
-	static Number *inst;
-};
-
-std::string Number::type = "decimal";
-Number *Number::inst = nullptr;
-
-struct Octal : Number
-{
-	friend struct Number;
-
-	void setValue(int in)
-	{
-		char buf[10];
-		sprintf(buf, "%o", in);
-		sscanf(buf, "%d", &value);
-	}
-protected:
-	Octal() {}
-};
-
-Number *Number::instance()
-{
-	if (!inst)
-		if (type == "octal")
-			inst = new Octal;
-		else
-			inst = new Number;
-	return inst;
+	if (n > 5) return NULL;
+	auto ntt = new int(888);
+	return ntt;
 }
 
 int main()
 {
-	Number::instance()->setValue(42);
-	std::cout << "Value is " << Number::instance()->getValue() << '\n';
+	int a = 5;
+	gsl::not_null<int *> p(&a);
 
-	Number::setType("octal");
-	Number::instance()->setValue(64);
-	std::cout << "Value is " << Number::instance()->getValue() << '\n';
+	std::cout << *p;
+
+	//p = nullptr;
 
 	std::cin.get();
 }
