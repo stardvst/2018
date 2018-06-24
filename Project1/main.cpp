0c2931b23f41a81444355deb2c265a42d3c090b3
Lilia <lilia.manukyan@hotmail.com>
2018-06-24 23:01:31 +0400

    c++ 17 structured bindings

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,30 +1,53 @@
+//#include <iostream>
+//
+//int foo(int inValue, int &outValue) {
+//	outValue = 2 * inValue;
+//	return inValue * 3;
+//}
+//
+//int main() {
+//
+//	int number = 4; // useless but necessary
+//	int answer = foo(5, number);
+//
+//	// design issue - which is invalue, which - outvalue
+//
+//	std::cin.get();
+//	return 0;
+//}
+
+//struct twoNumbers {
+//	int value1;
+//	int value2;
+//};
+//
+//twoNumbers fooStruct(int invalue) {
+//	return { invalue * 2, invalue * 3 };
+//}
+//
+//int main() {
+//	int number, answer;
+//	twoNumbers result = fooStruct(6);
+//	number = result.value1; // what value1?
+//	answer = result.value2; // what value2?
+//}
+
 #include <iostream>
+#include <tuple>
 
-struct Aclash
+std::tuple<int, int> fooTwo(int inValue)
 {
-	Aclash() : a(4) {}
-protected:
-	int a;
-};
-
-struct Bclash
-{
-	Bclash() : a(5) {}
-protected:
-	int a;
-};
-
-struct Cclash : Aclash, Bclash
-{
-	Cclash()
-	{
-		std::cout << Aclash::a;
-	}
-};
+	return { inValue * 2, inValue * 3 };
+}
 
 int main()
 {
-	Cclash c;
+	/*int number, answer;
+	std::tie(answer, number) = fooTwo(9);
+	*/
+
+	auto[answer, number] = fooTwo(9);
+	std::cout << answer << ' ' << number;
 
 	std::cin.get();
 }
