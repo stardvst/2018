20a1f0aae2867f7157f0389d567c7628e4398c2c
Lilia <lilia.manukyan@hotmail.com>
2018-06-26 21:14:50 +0400

    possible to call private virtual function

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,46 +1,37 @@
+//#include <iostream>
+//
+//template <typename It>
+//void dwim(It b, It e)
+//{
+//	while (b != e)
+//		/* typename std::iterator_traits<it>::value_type */ auto curValue = *b;
+//}
+//
+//int main()
+//{
+//	
+//
+//	std::cin.get();
+//}
+
 #include <iostream>
 
-struct Day
-{
-	explicit Day(int d) : val(d) {}
-	int val;
-};
-
-class Month
+class A
 {
 public:
-	static Month Jan() { return Month(1); }
-	static Month Feb() { return Month(2); }
-	static Month Mar() { return Month(3); }
+	virtual void f() { std::cout << "A" << std::endl; }
+};
 
+class B : public A
+{
 private:
-	explicit Month(int m) : val(m) {}
-	int val;
-};
-
-struct Year
-{
-	explicit Year(int y) : val(y) {}
-	int val;
-};
-
-class Date
-{
-public:
-	// error prone: Date(3, 4, 1990)... what?;
-	Date(int month, int day, int year);
-
-	// better!
-	Date(const Month &m, const Day &d, const Year &y);
+	void  f() override { std::cout << "B" << std::endl; }
 };
 
 int main()
 {
-	//Date d1(30, 3, 1995);
-
-	//Date d2(Day{ 30 }, Month{ 3 }, Year{ 1995 });
-
-	Date d3(Month::Mar(), Day{ 30 }, Year{ 1995 });
+	A* p = new B;
+	p->f();
 
 	std::cin.get();
 }
