062e89401aa9aefc18219ca55851e45679030381
Lilia <lilia.manukyan@hotmail.com>
2018-07-08 13:39:39 +0400

    class with 1 static member => sizeof==1

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,32 +1,15 @@
 #include <iostream>
-#include <memory>
-#include <map>
 
-template <typename It>
-void dwim(It b, It e)
+struct A
 {
-	while (b != e)
-		//typename std::iterator_traits<it>::value_type curValue = *b;
-		auto curValue = *b;
-}
-
-class Widget {};
+	static A inst;
+};
 
 int main()
 {
-	auto derefUPLess = [](const std::unique_ptr<Widget> &p1, const std::unique_ptr<Widget> &p2)
-	{
-		return *p1 < *p2;
-	};
-
-	auto derefLess = [](const auto &p1, const auto &p2) { return *p1 < *p2; };
-
-	std::map<std::string, int> m;
-	for (const std::pair<std::string, int> &p : m)
-	{
-		// wrong... pair type is std::pair<CONST std::string, int>
-		// => auto is better
-	}
+	A a;
+	std::cout << sizeof a;
 
 	std::cin.get();
+	return 0;
 }
