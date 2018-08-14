9b5dd0b1fdc5ca7d2014aceebc91968836e6615b
Lilia <lilia.manukyan@hotmail.com>
2018-08-14 22:33:02 +0400

    member function pointer sizeof

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
 
-struct VClass
+struct A
 {
-	int a;
-	int b;
-	virtual ~VClass() {}
+	void foo() const
+	{
+		std::cout << "A's this: " << std::showbase << std::internal << std::hex << this << '\n';
+	}
+	char data1[32];
 };
+
+struct B
+{
+	void bar() const
+	{
+		std::cout << "B's this: " << std::showbase << std::internal << std::hex << this << '\n';
+	}
+	char data2[64];
+};
+
+struct C : A, B {};
+
+void call_by_ptr(const C& obj, void (C::*mem_fn)() const)
+{
+	void *data[2];
+	std::memcpy(data, &mem_fn, sizeof(mem_fn));
+	std::cout << "------------------------------\n"
+		"Object ptr:\t" << &obj <<
+		"\nFptr:\t" << data[0] <<
+		"\nPtr adj:\t" << data[1] << '\n';
+	(obj.*mem_fn)();
+}
 
 int main()
 {
-	VClass obj;
+	C obj;
+//	call_by_ptr(obj, &C::foo);
+//	call_by_ptr(obj, &C::bar);
 
-	auto start_addr = reinterpret_cast<char *>(&obj);
-	auto a_addr = reinterpret_cast<char *>(&obj.a);
-
-	std::cout << "vptr is at " << (start_addr == a_addr ? "end" : "start") << '\n';
+	std::cout << sizeof(&C::foo);
 
 	std::cin.get();
 	return 0;
